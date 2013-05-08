f377c7611d2f44153f5ad01b60a586266abe7644
Jeff Hubbard <jhubbard@stelladot.com>
2013-05-08 05:53:56 -0700

    Fix a few minor quibbles and add docs to the injector

--- include/Deadline/Injector.php
+++ include/Deadline/Injector.php
@@ -7,23 +7,39 @@ use \RuntimeException,
 	\ReflectionClass;
 
 class Injector {
-	private $instances = [];
-	private $classcache = [];
-	private $namecache = [];
+	private $instances = [], $classcache = [], $namecache = [];
 
+	/**
+	 * Provide a dependency to the injector. You may make use of the dependency either by classname (of the object) or
+	 * by the provided name (in the case of a closure)
+	 * @param string [$name] The name of the dependency being provided
+	 * @param mixed [$obj] The actual object backing the dependency (which may be a closure)
+	 * @throws RuntimeException If the provided dependency name or class already exists
+	 */
 	public function provide($name, $obj) {
+		// store dependencies by class and by name, so you can match by either the typehint or the parameter name
 		$class = get_class($obj);
 		if(isset($this->namecache[$name])) throw new RuntimeException(sprintf('Duplicate dependency name %s', $name));
 		if(isset($this->classcache[$class])) throw new RuntimeException(sprintf('Duplicate dependency class %s', $class));
 		$this->namecache[$name] = $obj;
-		$this->classcache[$class] = $obj;
-	}
-	private function getProvider($name) {
-		if(isset($this->namecache[$name])) return $this->namecache[$name];
-		if(isset($this->classcache[$name])) return $this->classcache[$name];
-		return null;
+		// don't add closures to the class cache--it's only for concrete instances (which means this won't trip up the isset above, either)
+		if($class !== 'Closure') $this->classcache[$class] = $obj;
 	}
 
+	private function getProvider($name) {
+		// the class cache is more robust, so check it first
+		return isset($this->classcache[$name])) ? $this->classcache[$name] : isset($this->namecache[$name])) ? $this->namecache[$name] : null;
+	}
+
+	/**
+	 * Get an instance of the specified class, with the specified options
+	 * Supported options:
+	 * try - The first namespace to look for the class in
+	 * fallback - The second namespace to look for the class in
+	 * @param string [$class] The name of the class (either full or partial) to look up
+	 * @param array [$options] The options
+	 * @return mixed
+	 */
 	public function get($class, array $options = []) {
 		if(!isset($this->instances[$class])) {
 			$instance = $this->create($class, $options);
@@ -33,38 +49,41 @@ class Injector {
 		}
 		return $instance;
 	}
+
+	/**
+	 * Create a new instance of the specified class, with the specified options
+	 * @see get
+	 * @param string [$class] The name of the class (either full or partial) to look up
+	 * @param array [$options] The options
+	 * @return mixed
+	 */
 	public function create($class, array $options = []) {
 		$options = array_merge(['try' => 'Deadline', 'fallback' => 'Deadline'], $options);
-		$tryNamespace = $options['try'];
-		$fallbackNamespace = $options['fallback'];
 		$fqn = $class;
 		$instance = null;
 
-		if(!class_exists($fqn)) $fqn = $tryNamespace . '\\' . $class;
-		if(!class_exists($fqn)) $fqn = $fallbackNamespace  . '\\' . $class;
+		if(!class_exists($fqn)) $fqn = $options['try'] . '\\' . $class;
+		if(!class_exists($fqn)) $fqn = $options['fallback']  . '\\' . $class;
 		if(!class_exists($fqn)) throw new RuntimeException(sprintf('Class %1$s not found, tried namespaces: %1$s, %2$s\\%1$s, %3$s\\%1$s',
-			$class, $tryNamespace, $fallbackNamespace));
+			$class, $options['try'], $options['fallback']));
+
 		$ref = new ReflectionClass($fqn);
 		$ctor = $ref->getConstructor();
+
 		$args = [];
 		if($ctor !== null) {
+			// if the class has a ctor, inject the dependencies it wants
 			foreach($ctor->getParameters() as $parameter) {
 				$dependency = null;
 				// try by typehint first, if that fails try by name
 				$name = $parameter->getClass()->getName();
-				if($name != null) {
-					$dependency = $this->getProvider($name);
-				}
-				if($dependency === null) {
-					$dependency = $this->getProvider($parameter->getName());
-				}
-				if($dependency != null) {
-					if(is_callable($dependency)) {
-						$dependency = $dependency();
-					}
+				if($name !== null) $dependency = $this->getProvider($name);
+				if($dependency === null) $dependency = $this->getProvider($parameter->getName());
+
+				if($dependency !== null) {
+					if(is_callable($dependency)) $dependency = $dependency();
 					$args[$parameter->getPosition()] = $dependency;
 				} else {
-					var_dump($this);
 					throw new RuntimeException(sprintf('Could not satisfy dependency %s for class %s', $parameter->getName(), $class));
 				}
 			}
